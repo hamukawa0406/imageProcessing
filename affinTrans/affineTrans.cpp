19697c2bc4fb4d6dd8d8bbdd1a4d23b847396d94
YukiFujitsu <hamukawa0406@gmail.com>
2019-11-14 10:52:11 +0900

    affine Transformation

--- affinTrans/affineTrans.cpp
+++ affinTrans/affineTrans.cpp
@@ -30,13 +30,19 @@ affineTrans::affineTrans(struct pointD s, double r, struct point p):
 struct point affineTrans::getSrcPoint(const struct point p, const struct point srcImageSize){
     struct point srcP {0, 0};
     struct point dstImgSize {srcImageSize.x / 2, srcImageSize.y / 2};
-    srcP.x = (int)((scale.y*cos(rotate)*p.x + sin(rotate)*p.y
+    srcP.x =  (int)(((double)e*(p.x - dstImgSize.x) - b*(p.y - dstImgSize.y) + b*f - c*e)
+            /(a*e - b*d) + dstImgSize.x);
+/*
+    (int)((scale.y*cos(rotate)*p.x + sin(rotate)*p.y
                    - (paraTrans.x + dstImgSize.x)*scale.y*cos(rotate) - (paraTrans.y + dstImgSize.y)*sin(rotate) + scale.x*dstImgSize.x)
                   / (scale.x*scale.y*cos(rotate)*cos(rotate) + sin(rotate)*sin(rotate)));
-    //(int)(((double)e*p.x - b*p.y + b*f - c*e)/(a*e - b*d));
-    srcP.y = (int)(((-1)*sin(rotate)*p.x + scale.x*cos(rotate)*p.y
+//*/
+    srcP.y =  (int)(((double)-1*d*(p.x - dstImgSize.x) + a*(p.y - dstImgSize.y) + c*d - a*f + dstImgSize.y)
+           /(a*e - b*d) + dstImgSize.x);
+/*
+    (int)(((-1)*sin(rotate)*p.x + scale.x*cos(rotate)*p.y
                   + (paraTrans.y - dstImgSize.y)*scale.x*cos(rotate) + (dstImgSize.x - paraTrans.x)*sin(rotate) + scale.y*dstImgSize.y)
                   / (scale.x*scale.y*cos(rotate)*cos(rotate) + sin(rotate)*sin(rotate))) ;
-    //(int)(((double)-1*d*p.x + a*p.y + c*d - a*f)/(a*e - b*d));
+//*/
     return srcP;
 }
