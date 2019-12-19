1a5a0014551adaeacbaa026295d30a237fde7c73
YukiFujitsu <hamukawa0406@gmail.com>
2019-12-19 09:04:11 +0900

    Changes to be committed: 	deleted:    topic9/Lenna.ppm 	modified:   topic9/filter.cpp 	modified:   topic9/filter.h 	modified:   topic9/themain.cpp

--- topic9/filter.cpp
+++ topic9/filter.cpp
@@ -17,45 +17,66 @@ std::array<std::array<const double, filter::size>, filter::size>  filter::sharp 
 }};
 
 
-filter::filter(): select {5}, rank{5}, filt {}
-{
-}
 
-RGBColor filter::filtering(InctImage *src, int i, int j){
+filter::filter(InctImage &_simg, InctImage &_dimg, int inSelc): select{inSelc}, rank{}, filt{}, srcImg{_simg}, dstImg{_dimg}
+{
+    int inRank;
     switch(select){
-        case 1: //rank
-            return rankFilter(src, i, j);
-        case 2: //median
-            return medianFilter(src, i, j);
-        case 3: //mode
-            return modeFilter(src, i, j);    
-        case 4: //gaussian
-            return gaussFilter(src, i, j);
-        case 5: //sharp
-            return sharpFilter(src, i, j);
+        case 1:
+            do{
+                std::cout << "ランク値(0~8):" << std::endl;
+                std::cin >> inRank;
+            }while(inRank < 0 && inRank >= 9);
+            setRank(inRank);
+        case 2:
+            setRank(5);
+            break;
     }
 }
-RGBColor filter::rankFilter(InctImage *src, int i, int j){
-    std::array<RGBColor, size*size> rgbArr;
-    areaSort(src, rgbArr, i, j);
-    return rgbArr.at(rank);
-}
-RGBColor filter::medianFilter(InctImage *src, int i, int j){
-    return rankFilter(src, i, j);
+
+void filter::setRank(int _rank){
+    rank = _rank;
 }
 
-std::array<RGBColor, 9>::iterator filter::__upper_bound(std::array<RGBColor, size*size>::iterator first, std::array<RGBColor, size*size>::iterator second, unsigned char dens){
+RGBColor filter::filtering(int i, int j){
+    switch(select){
+        case 1: //rank
+            dstImg.setFileName("rank.pgm");
+            return rankFilter(i, j);
+        case 2: //median
+            dstImg.setFileName("median.pgm");
+            return medianFilter(i, j);
+        case 3: //mode
+            dstImg.setFileName("mode.pgm");
+            return modeFilter(i, j);    
+        case 4: //gaussian
+            dstImg.setFileName("gaussian.pgm");
+            return gaussFilter(i, j);
+        case 5: //sharp
+            dstImg.setFileName("sharp.pgm");
+            return sharpFilter(i, j);
+    }
+}
+RGBColor filter::rankFilter(int i, int j){
+    std::array<RGBColor, size*size> rgbArr;
+    areaSort(rgbArr, i, j);
+    return rgbArr.at(rank);
+}
+RGBColor filter::medianFilter(int i, int j){
+    return rankFilter(i, j);
+}
+
+std::array<RGBColor, 9>::iterator filter::__upper_bound(std::array<RGBColor, size*size>::iterator first, std::array<RGBColor, size*size> &arr, unsigned char dens){
     auto next = first+1;
-	for (auto itr = first; itr == next && next != second; itr++) {
-		next = itr + 1;
+	for (auto itr = first; itr->Dens == next->Dens && (itr+1) != arr.end(); ++itr, ++next) {
     }
 	return next;
 }
 
-RGBColor filter::modeFilter(InctImage *src, int i, int j){
+RGBColor filter::modeFilter(int i, int j){
     std::array<RGBColor, size*size> rgbArr;
     RGBColor mode;
-    areaSort(src, rgbArr, i, j);
+    areaSort(rgbArr, i, j);
     int n{}, count {1};
     for(auto itr = std::adjacent_find(rgbArr.begin(), rgbArr.end(), [](const RGBColor& x, const RGBColor& y){return x.Dens == y.Dens;}),
             last = rgbArr.end(), next = rgbArr.end();
@@ -63,23 +84,24 @@ RGBColor filter::modeFilter(InctImage *src, int i, int j){
     ){
         //std::cout << (int)itr->Dens << std::endl;
         //next = std::upper_bound(itr, last, itr->Dens, [](const RGBColor& x, const RGBColor& y){return x.Dens < y.Dens;});
-        next = __upper_bound(itr, last, itr->Dens);
+        next = __upper_bound(itr, rgbArr, itr->Dens);
         count = std::distance(itr, next);
         if(n < count){
             n = count;
             mode = *itr;
+            std::cout << "mode n " << (int)mode.Dens << " " << n << std::endl;
         }
         itr = std::adjacent_find(next, last, [](const RGBColor& x, const RGBColor& y){return x.Dens == y.Dens;});
     }
     return mode;
 }
 
-void filter::areaSort(InctImage *src, std::array<RGBColor, size*size> &arr,int i, int j){
+void filter::areaSort(std::array<RGBColor, size*size> &arr,int i, int j){
     RGBColor tRGB;
     auto itr = arr.begin();
     for(int y = 0; y < 3; y++){
         for(int x = 0; x < 3; x++){
-            tRGB = src->getPnmPixel(i+x-1, j+y-1);
+            tRGB = srcImg.getPnmPixel(i+x-1, j+y-1);
             *itr = tRGB;
             ++itr;
         }
@@ -89,13 +111,13 @@ void filter::areaSort(InctImage *src, std::array<RGBColor, size*size> &arr,int i
               {return x.Dens < y.Dens;});
 }
 
-RGBColor filter::gaussFilter(InctImage *src, int i, int j){
+RGBColor filter::gaussFilter(int i, int j){
     RGBColor tRGB;
     double dens {};
     double sumDens {};
     for(int y = 0; y < 3; y++){
         for(int x = 0; x < 3; x++){
-            tRGB = src->getPnmPixel(i+x-1, j+y-1);
+            tRGB = srcImg.getPnmPixel(i+x-1, j+y-1);
             dens = (double)tRGB.Dens*gaussian[x][y];
             sumDens += dens;
 //            std::cout << gaussian[x][y] << std::endl;
@@ -108,13 +130,13 @@ RGBColor filter::gaussFilter(InctImage *src, int i, int j){
     return tRGB;
 }
 
-RGBColor filter::sharpFilter(InctImage *src, int i, int j){
+RGBColor filter::sharpFilter(int i, int j){
     RGBColor tRGB;
     double dens {};
     double sumDens {};
     for(int y = 0; y < 3; y++){
         for(int x = 0; x < 3; x++){
-            tRGB = src->getPnmPixel(i+x-1, j+y-1);
+            tRGB = srcImg.getPnmPixel(i+x-1, j+y-1);
             dens = tRGB.Dens*sharp[x][y];
             sumDens += dens;
         }
--- topic9/filter.h
+++ topic9/filter.h
@@ -7,23 +7,24 @@ class filter {
         int select;
         int rank;
         static const int size {3};
-        //InctImage srcImg;
+        InctImage& srcImg;
+        InctImage& dstImg;
         std::array<std::array<int, size>, size> filt; 
         std::array<std::array<const double, size>, size> static gaussian;
         std::array<std::array<const double, size>, size> static sharp; 
     public:
-        filter();
-        filter(int);
+        filter(InctImage& _simg, InctImage& _dimg, int inSelc);
+        void setRank(int _rank);
         int getFilter(int i, int j);
-        RGBColor filtering(InctImage *src, int i, int j);
-        RGBColor rankFilter(InctImage *src, int i, int j);
-        RGBColor medianFilter(InctImage *src, int i, int j);
-        RGBColor modeFilter(InctImage *src, int i, int j);
-        RGBColor gaussFilter(InctImage *src, int i, int j);
-        RGBColor sharpFilter(InctImage *src, int i, int j);
+        RGBColor filtering(int i, int j);
+        RGBColor rankFilter(int i, int j);
+        RGBColor medianFilter(int i, int j);
+        RGBColor modeFilter(int i, int j);
+        RGBColor gaussFilter(int i, int j);
+        RGBColor sharpFilter(int i, int j);
         int calcFilter(int x, int y);
-        void areaSort(InctImage *src, std::array<RGBColor, size*size> &arr, int i, int j);
-        std::array<RGBColor, 9>::iterator __upper_bound(std::array<RGBColor, size*size>::iterator first, std::array<RGBColor, size*size>::iterator second,
+        void areaSort(std::array<RGBColor, size*size> &arr, int i, int j);
+        std::array<RGBColor, 9>::iterator __upper_bound(std::array<RGBColor, size*size>::iterator first, std::array<RGBColor, size*size>& arr,
                             unsigned char dens);
         
         
