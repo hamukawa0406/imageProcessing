c75185ea38cde87fbf061da65e6ff3ffecfda424
Hamukawa <hamukawa0406@gmail.com>
2020-01-06 16:44:17 +0900

    setHist method tukure!

--- topic1/1-3/statistic.cpp
+++ topic1/1-3/statistic.cpp
@@ -1,37 +1,97 @@
 #include "ifstream_string.h"
 #include "statistic.h"
+#include <map>
 
-Statistic::Statistic()
+Statistic::Statistic(InctImage& _img):
+    img{_img},
+    Lut{}
 {
-}
-
-Statistic::Statistic(InctImage _img): img{_img}
-{
-    ave = calcAve();
-    var = calcVar();
-    max = calcMax();
-    min = calcMin();
-    med = calcMed();
-    mode = calcMode();
-    setHist();
-}
-
-double Statistic::calcAve(){
-    double sum {};
+    int count {};
     RGBColor trgb;
+    sortedData.resize(img.getWidth()*img.getHeight());
     for(int j = 0; j < img.getHeight(); j++){
         for(int i = 0; i < img.getWidth(); i++){
             trgb = img.getPnmPixel(i, j);
-            sum += trgb.R;
+            sortedData[count] = trgb.R;
+            ++count;
         }
     }
-    return sum / ((double)img.getWidth()*img.getHeight());
+    std::sort(sortedData.begin(), sortedData.end());
+    setStatistic();
 }
+
+void Statistic::setStatistic(){
+    double sum {};
+    double powSum {};
+    int count {};
+    int cMax {};
+    unsigned char _mode;
+    for(auto x : sortedData){
+        sum += x;
+        powSum += pow(x, 2);
+        if(x == _mode){
+            ++count;
+            _mode = x;
+        }
+        else if(count > cMax){
+            mode = _mode;
+        }
+    }
+    ave = calcAve(sum);
+    var = calcVar(powSum);
+    min = sortedData.front();
+    max = sortedData.back();
+    med = sortedData.at(sortedData.size() / 2);
+
+}
+
+double Statistic::calcAve(double sum){
+    return sum / sortedData.size();
+}
+
+double Statistic::calcVar(double sum){
+    return sum - pow(ave, 2);
+} 
+
+void Statistic::makeLUT(unsigned int kaichou){
+	int k;
+	static int i = 0;
+	static int dosu = 0;
+	static int ichikaichou = (int)(256 / kaichou);
+	
+	for(k = 0; k < 256; ++k){
+		if(i < ichikaichou){
+	        Lut[k] = dosu;
+		    ++i;
+		}
+	    else{
+		    dosu += ichikaichou;
+		    i = 0;
+		}
+		
+	}
+}
+
+
+
+void Statistic::setHist(){
+    unsigned char preDens {0};
+    makeLUT(hist.size());
+    for(auto x = 0; x < sortedData.size(); ++x){
+        sortedData[x] = Lut[sortedData[x]];
+    }
+    for(auto x = 1; x < sortedData.size(); ++x){
+        if(preDens == sortedData[x]){
+
+        }
+    }
+}
+
 
 void Statistic::printHist(){
 
 }
-
+//*/
 double Statistic::getAve(){
     return ave;
 }
--- topic1/1-3/statistic.h
+++ topic1/1-3/statistic.h
@@ -1,30 +1,30 @@
 #include<iostream>
 #include<array>
+#include<algorithm>
 #include "ifstream_string.h"
 
 
 class Statistic
 {
 private:
-    InctImage img;
+    InctImage& img;
+    std::vector<unsigned char> sortedData;
     double ave;
     double var;
     double max;
     double min;
     double med;
     double mode;
-    double hist[8];
-    double calcAve();
-    double calcVar();
-    double calcMax();
-    double calcMin();
-    double calcMed();
-    double calcMode();
+    unsigned char Lut[256];
+    std::array<int, 8> hist;
+    void setStatistic();
+    double calcAve(double sum);
+    double calcVar(double sum);
+    void makeLUT(unsigned int kaichou);
     void setHist();
 public:
     Statistic();
-    Statistic(InctImage);
-    ~Statistic();
+    Statistic(InctImage & _img);
     double getAve();
     double getVar();
     double getMax();
