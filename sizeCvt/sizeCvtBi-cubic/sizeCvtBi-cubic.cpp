f1e3025af6b9f71656a04498073af6b97fac5162
Hamukawa <hamukawa0406@gmail.com>
2020-01-30 17:18:47 +0900

    image processing complete!!!

--- sizeCvt/sizeCvtBi-cubic/sizeCvtBi-cubic.cpp
+++ sizeCvt/sizeCvtBi-cubic/sizeCvtBi-cubic.cpp
@@ -9,9 +9,7 @@
 //バイキュービック補間法　　<- 死ぬ
 
 void sizeCvtBiCubic(struct ppmimg *src, struct ppmimg *dst, double scale);
-unsigned char min(unsigned char a, unsigned char b, unsigned char c);
-unsigned char max(unsigned char a, unsigned char b, unsigned char c);
-unsigned char roundAngle(unsigned char angle);
+unsigned char roundAngle(double angle);
 double myAbs(double a);
 
 char dstName[256] = {0};
@@ -51,7 +49,7 @@ int main(void){
 }
 
 void sizeCvtBiCubic(struct ppmimg *src, struct ppmimg *dst, double scale){
-    int a = -1;
+    int a = -1.5;
 	char _dstName[256];
 	sprintf(_dstName, dstName);
 
@@ -76,16 +74,17 @@ void sizeCvtBiCubic(struct ppmimg *src, struct ppmimg *dst, double scale){
             int color = 0;  // 補間する画素
 
             struct RGBColor color_element;
+            color_element.R = 0;
+            color_element.G = 0;
+            color_element.B = 0;
+            
                 
             // バイキュービックの処理範囲
             if (xBase >= 1 && xBase < src->iwidth - 2 && yBase >= 1 && yBase < src->iheight - 2) {
-                color_element.R = 0;
-                color_element.G = 0;
-                color_element.B = 0;
                        
                 // 基点の周辺16画素を取得して処理
-                for (int i = -1; i <= 2; i++) {
-                    for (int j = -1; j <= 2; j++) {
+                for (int j = -1; j <= 2; j++) {
+                    for (int i = -1; i <= 2; i++) {
                         // 実際に処理する画素を設定
                         int xCurrent = xBase + i;
                         int yCurrent = yBase + j;
@@ -100,10 +99,10 @@ void sizeCvtBiCubic(struct ppmimg *src, struct ppmimg *dst, double scale){
                         // まずはX座標の距離で重みを設定
     	                // 1以下、2以下のとき、それぞれ重み付け
                         if (distX <= 1.0) {
-                            weight = 1.0 - parameter[0]*distX*distX + parameter[1]*distX*distX*distX;
+                            weight = (1.0 - parameter[0]*distX*distX + parameter[1]*distX*distX*distX);
                         }
 						else if (distX <= 2.0) {
-                            weight = parameter[2] + parameter[3]*distX - parameter[4]*distX*distX + a*distX*distX*distX;
+                            weight = (parameter[2] + parameter[3]*distX - parameter[4]*distX*distX + a*distX*distX*distX);
                         }
 						else {
                             continue;  // 何も処理しないので、次のループへ
@@ -111,10 +110,10 @@ void sizeCvtBiCubic(struct ppmimg *src, struct ppmimg *dst, double scale){
                                         
                         // Y座標の距離で重みを設定
                         if (distY <= 1.0) {
-                            weight *= 1.0 - parameter[0]*distY*distY + parameter[1]*distY*distY*distY;
+                            weight *= (1.0 - parameter[0]*distY*distY + parameter[1]*distY*distY*distY);
                         }
 						else if (distY <= 2.0) {
-                            weight *= parameter[2] + parameter[3]*distY - parameter[4]*distY*distY + a*distY*distY*distY;
+                            weight *= (parameter[2] + parameter[3]*distY - parameter[4]*distY*distY + a*distY*distY*distY);
                         }
 						else {
                             continue;
@@ -125,9 +124,9 @@ void sizeCvtBiCubic(struct ppmimg *src, struct ppmimg *dst, double scale){
                         struct RGBColor color_process = getPnmPixel(src, xCurrent, yCurrent);
                                         
                         // 画素をRGB分割し、重みをかけて足し合わせる
-                        color_element.R += (unsigned char)(color_process.R*weight);
-                        color_element.G += (unsigned char)(color_process.G*weight);
-                        color_element.B += (unsigned char)(color_process.B*weight);
+                        color_element.R = roundAngle(color_element.R + color_process.R*weight);
+                        color_element.G = roundAngle(color_element.G + color_process.G*weight);
+                        color_element.B = roundAngle(color_element.B + color_process.B*weight);
                     }
                  //   putchar('\n');
                 }
@@ -149,35 +148,13 @@ void sizeCvtBiCubic(struct ppmimg *src, struct ppmimg *dst, double scale){
 	sprintf(dstName, _dstName);
 }
 
-//*/
-unsigned char min(unsigned char a, unsigned char b, unsigned char c)
-{
-	if(a < b){
-		if(a < c) return a;
-	}
-	else{
-		if(b < c) return b;
-	}
-	return c;
-}
-
-unsigned char max(unsigned char a, unsigned char b, unsigned char c)
-{
-	if(a > b){
-		if(a > c) return a;
-	}
-	else{
-		if(b > c) return b;
-	}
-	return c;
-}
-
-unsigned char roundAngle(unsigned char angle)
+unsigned char roundAngle(double angle)
 {
 	if(angle < 0)
-		return angle + 360;
-	else if(angle >= 360)
-		return angle - 360;
+		return 0;
+	else if(angle >= 256)
+        return 255;
+    else (unsigned char)angle;
 }
 
 double myAbs(double a){
